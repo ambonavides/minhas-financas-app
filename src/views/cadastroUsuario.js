09352d63fc613461be988581a122354bd75ebeaf
Anderson Bonavides <ambonavides@gmail.com>
2020-05-15 22:43:19 -0300

    cadastro de usuario

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -5,6 +5,11 @@ import FormGroup from '../ components/form-gruop';
 
 import { withRouter } from 'react-router-dom';
 
+import UsuarioService from '../app/service/usuarioservice';
+import { mensagemSucesso, mensagemErro } from '../ components/toastr';
+
+
+
 class CadastroUsuario extends React.Component{
 
     state = {
@@ -14,8 +19,56 @@ class CadastroUsuario extends React.Component{
         senhaRepeticao: '',
     }
 
+    constructor(){
+        super()
+        this.service = new UsuarioService();
+    }
+
+    validar(){
+        const msgs = []
+        if(!this.state.nome){
+            msgs.push('O campo nome é obrigatório.');
+        }
+
+        if(!this.state.email){
+            msgs.push('O campo email é obrigatório.');
+        }else if(!this.state.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)){
+            msgs.push('Infome um email válido.');
+        }
+
+        if(!this.state.senha || !this.state.senhaRepeticao){
+            msgs.push('Digite a senha 2x.')
+        }else if(this.state.senha !== this.state.senhaRepeticao){
+            msgs.push('As senhas não são igueis.')
+        }
+
+        return msgs;
+    }
+
     cadastrar = () =>{
-        console.log(this.state);
+        const msgs = this.validar();
+
+        if(msgs && msgs.length > 0){
+            msgs.forEach((msg, index) =>{
+                mensagemErro(msg);
+            });
+            return false;
+        }
+
+        const usuario = {
+            nome: this.state.nome,
+            email: this.state.email,
+            senha: this.state.senha            
+        }
+
+        this.service.salvar(usuario)
+        .then(response =>{
+            mensagemSucesso('Usuário cadastrado com sucesso!');
+            this.props.history.push('/login');
+        })
+        .catch(error =>{
+            mensagemErro(error.response.data);
+        });
     }
 
     cancelar = () =>{
